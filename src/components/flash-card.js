e8d99266531e7bf16514d426309d26488ec911af
Tyler Palmer <mr.tylerpalmer@gmail.com>
2016-04-15 18:42:51 -0700

    initial setup

---
+++ src/components/flash-card.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class FlashCard extends Component {
+  render() {
+    return (
+      <div>FlashCard</div>
+    )
+  }
+}
+
+export default FlashCard;
---
+++ src/index.js
@@ -0,0 +1,12 @@
+// find react library and attach to variable React
+import React from 'react';
+
+// create new component. should produce some html.
+
+
+const App = function() {
+  return <div>Hi!</div>;
+};
+// Take this component HTML and put it on page / in the DOM
+
+React.render(App);
