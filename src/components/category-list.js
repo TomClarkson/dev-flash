144be268510f7807f9c119efdc33fbf1c2bc6440
Tyler Palmer <mr.tylerpalmer@gmail.com>
2016-04-27 11:55:02 -0700

    setup for categorylist component

--- src/components/category-list.js
+++ src/components/category-list.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
+import CategoryListItem from './category-list-item';
 
 class CategoryList extends Component {
+  renderList() {
+    return this.props.categories.map(category => {
+      return (
+        <li onClick={() => this.props.selectCategory(category)} key={category.name}>{category.name}</li>
+      );
+    })
+  }
+
   render() {
-    return <div>CategoryList</div>;
+    return <ul>CategoryList</ul>;
   }
 }
 
